55f1de3bfc4e30ab8838c14e242932e2280e5f2c
Angeluz20 <66801548+Angeluz20@users.noreply.github.com>
2022-09-16 17:54:00 -0300

    feat: Firebase Connection pronta

---
+++ src/pages/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import 'firebase/firebase-firestore'
+import 'firebase/auth'
+const firebaseConfig = {
+    apiKey: "AIzaSyDzjlUuv7OTu88PCifZiSXq5YGbdpljASU",
+    authDomain: "meucurriculoonline-8c9b3.firebaseapp.com",
+    databaseURL: "https://meucurriculoonline-8c9b3-default-rtdb.firebaseio.com",
+    projectId: "meucurriculoonline-8c9b3",
+    storageBucket: "meucurriculoonline-8c9b3.appspot.com",
+    messagingSenderId: "764250081867",
+    appId: "1:764250081867:web:9a5249c3a4d83abd219bce"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+export default firebase;
