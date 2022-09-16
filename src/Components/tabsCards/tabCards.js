b1517ba5d30d46168fe3d7617a21da9215a5bf0e
Angeluz20 <66801548+Angeluz20@users.noreply.github.com>
2022-09-16 17:56:26 -0300

    feat: Componente Tabs da tela home pronta.

---
+++ src/Components/tabsCards/tabCards.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Container, Row, Tabs, Tab } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../tabsCards/styles.css'
+
+export default function tabsCards({ cabecalho, body, xp,objective }) {
+  return (
+    <Container className='py-4'>
+
+      <Row className='justify-content-center'>
+        <Tabs justify variant='pills' defaultActiveKey={'tab-1'} className='mb-1 p-0' >
+          <Tab eventKey='tab-1' title='Perfil' >
+            <div id='colorLabelText'>
+              {cabecalho}
+            </div>
+          </Tab>
+           <Tab eventKey='tab-2' title='Objetivo'>
+            <div id='colorLabelText'>
+              {objective}
+            </div>
+          </Tab>
+          <Tab eventKey='tab-3' title='Formação'>
+            <div id='colorLabelText'>
+              {body}
+            </div>
+          </Tab>
+          <Tab eventKey='tab-4' title='Experiência'>
+            <div id='colorLabelText'>
+              {xp}
+            </div>
+          </Tab>
+         
+        </Tabs>
+
+      </Row>
+    </Container>
+  );
+}
