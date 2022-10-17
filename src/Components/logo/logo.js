1e9387823a299f0e885df0758c848c6ebb1f8412
Angeluz20 <66801548+Angeluz20@users.noreply.github.com>
2022-10-16 23:01:09 -0300

    Componente Logo criado

---
+++ src/Components/logo/logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../logo/styles.css'
+export default function logo() {
+    return (
+        <div>
+            <div className='name-logo'>
+                <div className='name-meu-curriculo'>
+                    <h5 id='textMy'>MEU</h5>
+                    <h5 id='textCurri'>CURRÍCULO</h5>
+                </div>
+
+                <h6 id='textOn'>ONLINE</h6>
+            </div>
+        </div>
+    );
+}
