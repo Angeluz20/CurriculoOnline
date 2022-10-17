c44d68cd5605f1af85e97d966965d8a533bb0dd1
Angeluz20 <66801548+Angeluz20@users.noreply.github.com>
2022-10-16 22:52:07 -0300

    Index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,16 @@ import Route from './Route'
 import Signin from "../Signin/signin";
 import SignUp from "../pages/SignUp/SignUp";
 import Home from '../pages/home/home'
+import Profile from '../pages/profile/profile'
+import EditCurriculo from "../pages/editCurriculo/edit";
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={Signin} />
             <Route exact path="/register" component={SignUp} />
             <Route exact path="/home" component={Home} isPrivate />
+            <Route exact path="/profile" component={Profile} isPrivate />
+            <Route exact path="/edit" component={EditCurriculo } isPrivate />
         </Switch>
     )
 }
