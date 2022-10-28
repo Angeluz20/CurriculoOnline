32134d5c2056c2cdf6c11e82553c591094947642
Angeluz20 <66801548+Angeluz20@users.noreply.github.com>
2022-10-28 13:41:48 -0300

    montando o template 3, sem funcionalidades e sem estilizações

---
+++ src/Components/templates/temp3/temp3.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../temp3/temp3.css'
+
+export default function Templete3() {
+  return (
+    <div className='template3'   >
+
+      <div className='design-temp3'>
+      </div>
+      <div>
+
+      </div>
+      <div className='header-template-3' >
+
+
+        <div className='data-user-header'>
+          <div className='nome-temp3'>
+            <h5>Nome do usuario</h5>
+          </div>
+
+          <div className='info-user-temp3'>
+            <span><strong>Celular</strong> (00) 0 0000-0000</span>
+            <span><strong>Celular</strong> (00) 0 0000-0000</span>
+            <span><strong>E-mail</strong> user@gmail.com</span>
+            <span><strong>Cidade</strong> Manaus-Am</span>
+            <span><strong>Endereço</strong> Travessa Fantasia</span>
+          </div>
+        </div>
+        <div className='photo-template'></div>
+
+      </div>
+      <div>
+
+      </div>
+
+    </div>
+  );
+}
